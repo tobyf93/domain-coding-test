c56c85ba5d168cbc7b9df77fe0353075a7411df1
Toby Flemming <tobyf93@hotmail.com>
2017-06-24 13:21:50 +1000

    Add elements to HTML.  Define global styles and colour file.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import configureStore from './redux/store';
 import Builder from './containers/Builder';
+import './assets/stylesheets/_global.scss';
 
 const store = configureStore();
 
--- webpack.config.js
+++ webpack.config.js
@@ -38,4 +38,7 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new ExtractTextPlugin('main.css'),
+  ],
 };
